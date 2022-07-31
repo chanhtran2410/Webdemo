64a805f08ea997a4eb062535f9f63fe772a1fbc3
chanhtran2410 <tranletrungchanh12345@gmail.com>
2022-07-31 22:55:47 +0700

    update shop.html

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,34 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 		$(document).delegate('.open', 'click', function(event){
 			$(this).addClass('oppenned');
+			$(this).removeClass('open');
+			event.stopPropagation();
+		})
+		$(document).delegate('.oppenned', 'click', function(event){
+			$(this).addClass('open');
+			$(this).removeClass('oppenned');
 			event.stopPropagation();
 		})
 		$(document).delegate('body', 'click', function(event) {
-			$('.open').removeClass('oppenned');
+			$('.oppenned').addClass('open')
+			$('.oppenned').removeClass('oppenned');
 		})
 		$(document).delegate('.cls', 'click', function(event){
-			$('.open').removeClass('oppenned');
+			$('.oppenned').addClass('open')
+			$('.oppenned').removeClass('oppenned');
 			event.stopPropagation();
 		});
 	});
- 
+ */
+
+const toggle =document.querySelector(".open");
+
+toggle.addEventListener("click", function(){
+	if(toggle.classList.contains("oppenned")){
+		toggle.classList.remove("oppenned");
+		toggle.classList.add("open")
+	}else{
+		toggle.classList.remove("open");
+		toggle.classList.add("oppenned")
+	}
+})
